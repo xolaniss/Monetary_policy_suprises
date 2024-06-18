eb67464730981cf1fc9b652efe9ccd9bc57877c8
Xolani Sibande <xolaniss@gmail.com>
2024-06-18 16:07:18 +0200

    todo list

---
+++ Scripts/TODO.R
@@ -0,0 +1,10 @@
+# Description
+# To do list for paper
+
+# List
+# 1. Data collection
+## daily repo data
+## daily stock market, exchange rate, commodity prices, bond market, corporate debt, and FRAs
+## refinitiv intra day data on policy announcement days 
+
+
